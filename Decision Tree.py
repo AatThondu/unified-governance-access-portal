adc0f868f20abe1798c3ec62d73a8e9814e17ba5
AatThondu <49188138+AatThondu@users.noreply.github.com>
2019-05-07 09:45:11 +0530

    Update Decision Tree.py

--- Decision Tree.py
+++ Decision Tree.py
@@ -1,12 +1,5 @@
-"""
-Implementation of a basic regression decision tree.
-Input data set: The input data set must be 1-dimensional with continuous labels.
-Output: The decision tree maps a real number input to a real number output. 
-"""
 from __future__ import print_function
-
 import numpy as np
-
 class Decision_Tree:
     def __init__(self, depth = 5, min_leaf_size = 5):
         self.depth = depth
@@ -15,7 +8,6 @@ class Decision_Tree:
         self.right = None
         self.min_leaf_size = min_leaf_size
         self.prediction = None
-
     def mean_squared_error(self, labels, prediction):
         """
         mean_squared_error:
@@ -25,9 +17,7 @@ class Decision_Tree:
         """
         if labels.ndim != 1:
             print("Error: Input labels must be one dimensional")
-
         return np.mean((labels - prediction) ** 2)
-
     def train(self, X, y):
         """
         train:
@@ -36,7 +26,6 @@ class Decision_Tree:
         The contents of y are the labels for the corresponding X values
         train does not have a return value
         """
-
         """
         this section is to check that the inputs conform to our dimensionality constraints
         """
@@ -49,19 +38,14 @@ class Decision_Tree:
         if y.ndim != 1:
             print("Error: Data set labels must be one dimensional")
             return
-
         if len(X) < 2 * self.min_leaf_size:
             self.prediction = np.mean(y)
             return
-
         if self.depth == 1:
             self.prediction = np.mean(y)
             return
-
         best_split = 0
         min_error = self.mean_squared_error(X,np.mean(y)) * 2
-
-
         """
         loop over all possible splits for the decision tree. find the best split.
         if no split exists that is less than 2 * error for the entire array
@@ -79,13 +63,11 @@ class Decision_Tree:
                 if error < min_error:
                     best_split = i
                     min_error = error
-
         if best_split != 0:
             left_X = X[:best_split]
             left_y = y[:best_split]
             right_X = X[best_split:]
             right_y = y[best_split:]
-
             self.decision_boundary = X[best_split]
             self.left = Decision_Tree(depth = self.depth - 1, min_leaf_size = self.min_leaf_size)
             self.right = Decision_Tree(depth = self.depth - 1, min_leaf_size = self.min_leaf_size)
@@ -93,9 +75,7 @@ class Decision_Tree:
             self.right.train(right_X, right_y)
         else:
             self.prediction = np.mean(y)
-
         return
-
     def predict(self, x):
         """
         predict:
@@ -113,7 +93,6 @@ class Decision_Tree:
         else:
             print("Error: Decision tree not yet trained")
             return None
-
 def main():
     """
     In this demonstration we're generating a sample data set from the sin function in numpy.
@@ -122,14 +101,11 @@ def main():
     """
     X = np.arange(-1., 1., 0.005)
     y = np.sin(X)
-
     tree = Decision_Tree(depth = 10, min_leaf_size = 10)
     tree.train(X,y)
-
     test_cases = (np.random.rand(10) * 2) - 1
     predictions = np.array([tree.predict(x) for x in test_cases])
     avg_error = np.mean((predictions - test_cases) ** 2)
-
     print("Test values: " + str(test_cases))
     print("Predictions: " + str(predictions))
     print("Average error: " + str(avg_error))
